2c789eedf3cbfd470dbeade283e714c4da338e1b
BrownQi <brownqi@foxmail.com>
2020-03-22 00:02:49 +0800

    BaseDao

--- book/src/cn/brownqi/dao/impl/BaseDao.java
+++ book/src/cn/brownqi/dao/impl/BaseDao.java
@@ -2,9 +2,13 @@ package cn.brownqi.dao.impl;
 
 import cn.brownqi.utils.JdbcUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @Description:
@@ -18,19 +22,79 @@ public abstract class BaseDao {
 
     /**
      * update() 方法用来执行：insert、update、delete语句
+     *
      * @return 如果返回-1，说明执行失败。返回其他表示影响的行数
      */
-    public int update(String sql,Object ... args){
+    public int update(String sql, Object... args) {
 
         Connection conn = JdbcUtils.getConnection();
         try {
-            queryRunner.update(conn,sql,args);
+            return queryRunner.update(conn, sql, args);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn);
         }
         return -1;
     }
 
+    /**
+     * 查询返回一个JavaBean的sql语句
+     *
+     * @param type 返回的对象类型
+     * @param sql  执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T>  返回的类型的泛型
+     * @return
+     */
+    public <T> T queryForOne(Class<T> type, String sql, Object... args) {
+        Connection conn = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(conn, sql, new BeanHandler<T>(type));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn);
+        }
+        return null;
+    }
 
+    /**
+     * 查询返回多个JavaBean的sql语句
+     *
+     * @param type 返回的对象类型
+     * @param sql  执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T>  返回的类型的泛型
+     * @return
+     */
+    public <T> List<T> queryForList(Class<T> type, String sql, Object... args) {
+        Connection conn = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(conn, sql, new BeanListHandler<T>(type));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn);
+        }
+        return null;
+    }
+
+    /**
+     * 执行返回一行一列的sql语句
+     *
+     * @param sql 执行的sql语句
+     * @param args sql对应的参数值
+     * @return
+     */
+    public Object queryForSingleValue(String sql, Object... args) {
+        Connection conn = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(conn, sql, new ScalarHandler(), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
